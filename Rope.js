c89d0ae1672b923cb8bbcf28b3ff0a5581d649a6
Ayush111200F <69429020+Ayush111200F@users.noreply.github.com>
2020-09-06 11:22:00 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,34 @@
+class Rope{
+    constructor(body1, body2, offsetX, offsetY){
+        this.offsetX = offSetX;
+        this.offsetY = offSetY;
+        var options = {
+            bodyA: body1,
+            bodyB: body2,
+            pointB: {x:this.offsetx, y:this.offsetY}
+            
+        }
+        
+        //this.pointB = pointB
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+   
+
+    display(){
+        //image(this.sling1,200,20);
+        //image(this.sling2,170,20);
+        if(this.sling.bodyA){
+            var pointA = this.rope.bodyA.position;
+            //var pointB = this.pointB;
+             var pointB = this.rope.bodyB.position;
+            var anchor1X = pointA.x
+            var anchor1Y = pointA.y;
+            var anchor2X = pointB.x;
+            var anchor2Y = pointB.y;
+            line(anchor1X, anchor1Y, anchor2X, anchor2Y);
+           
+        }
+    }
+    
+}
---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var bobObject1, bobObject2, bobObject3, bobObject4, bobObject5;
+var roofObject1;
+var ropeOject1;
+
+//function preload()
+//{
+	
+//}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	bobObject1 = new Bob(300,500,30);
+	bobObject2 = new Bob(200,300,30);
+	bobObject3 = new Bob(350,200,30);
+	bobObject4 = new Bob(250,100,30);
+	bobObject5 = new Bob(378,150,30);
+
+	roofObject1 = new Roof(300,100,400,20);
+
+	rope1 = new rope(bobObject1.body, roofObject1.body, 20, 0);
+
+
+	
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  //rectMode(CENTER);
+  Engine.update(engine);
+  background(0);
+
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+  roofObject1.display();
+  rope1.display();
+  
+  drawSprites();
+ 
+}
+
+
+
